d83e5aa51a75b1897f7fa4385c8edd8224023533
Seamus Connolly <sghconnolly@gmail.com>
2023-12-25 14:36:41 +0000

    Add throughput capture

--- project.py
+++ project.py
@@ -6,7 +6,7 @@ def main():
     name = get_valid_team_name()
     save_team_data_to_csv(name)
     sprint_details = get_sprint_details()
-    print("Sprint Date:", f"{sprint_details}")
+    print("Sprint Details:", f"{sprint_details}") # Delete when ready
 
 
 def get_valid_team_name():
@@ -23,27 +23,36 @@ def get_valid_team_name():
             print("Invalid response. Please enter 'Y' to confirm")
 
 
-def save_team_data_to_csv(team_name, file_path="team_data.csv"):
-    with open(file_path, "w", newline="") as csvfile:
-        writer = csv.writer(csvfile)
-        # Save team name as a single string in the first cell
-        writer.writerow([team_name])
-
-
 def get_sprint_details():
     # Get sprint start date from the user
     while True:
         try:
             start_date_str = input("Enter sprint start date (YYYY-MM-DD): ")
             start_date = datetime.strptime(start_date_str, "%Y-%m-%d")
-            print(start_date)
             break
         except ValueError:
             print("Invalid date format. Please enter date in YYYY-MM-DD format.")
 
+    # Get throughput from the user
+    while True:
+        try:
+            throughput = int(input("Enter throughput (number of tasks completed): "))
+            break
+        except ValueError:
+            print("Invalid input. Please enter a valid integer for throughput.")
+
+
     return {
         "sprint_start_date": start_date.strftime("%Y-%m-%d"),
+        "sprint_throughput": throughput,
         }
+
+
+def save_team_data_to_csv(team_name, file_path="team_data.csv"):
+    with open(file_path, "w", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        # Save team name as a single string in the first cell
+        writer.writerow([team_name])
 
 
 if __name__ == "__main__":
