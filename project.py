21c0094e08ee2f0eb600749a5e5265cb4785701b
Seamus Connolly <sghconnolly@gmail.com>
2023-12-25 14:22:31 +0000

    Add sprint start date

--- project.py
+++ project.py
@@ -1,9 +1,12 @@
 import csv
+from datetime import datetime
 
 
 def main():
     name = get_valid_team_name()
     save_team_data_to_csv(name)
+    sprint_details = get_sprint_details()
+    print("Sprint Date:", f"{sprint_details}")
 
 
 def get_valid_team_name():
@@ -25,6 +28,22 @@ def save_team_data_to_csv(team_name, file_path="team_data.csv"):
         writer = csv.writer(csvfile)
         # Save team name as a single string in the first cell
         writer.writerow([team_name])
+
+
+def get_sprint_details():
+    # Get sprint start date from the user
+    while True:
+        try:
+            start_date_str = input("Enter sprint start date (YYYY-MM-DD): ")
+            start_date = datetime.strptime(start_date_str, "%Y-%m-%d")
+            print(start_date)
+            break
+        except ValueError:
+            print("Invalid date format. Please enter date in YYYY-MM-DD format.")
+
+    return {
+        "start_year": start_date.strftime("%Y-%m-%d")
+        }
 
 
 if __name__ == "__main__":
