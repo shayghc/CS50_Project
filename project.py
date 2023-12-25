51f3947710ec32f62c69d710aa1d8538de7a92df
Seamus Connolly <sghconnolly@gmail.com>
2023-12-25 10:12:20 +0000

    Create csv file

--- project.py
+++ project.py
@@ -1,5 +1,9 @@
+import csv
+
+
 def main():
-    get_team_name()
+    name = get_team_name()
+    save_team_data_to_csv(name)
 
 
 def get_team_name():
@@ -10,6 +14,13 @@ def get_team_name():
         confirmation = input(f"Is {team_name} the correct name? (Y/N)")
         response = confirmation.lower()
     return team_name
+
+
+def save_team_data_to_csv(team_name, file_path="team_data.csv"):
+    with open(file_path, "w", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        # Save team name as a single string in the first cell
+        writer.writerow([team_name])
 
 
 if __name__ == "__main__":
